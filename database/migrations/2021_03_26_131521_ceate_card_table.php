07c90e7fc017a78416ca5e3629ad6829b369183e
mirco verderosa <verderosamircowork@gmail.com>
2021-03-26 14:32:23 +0100

    Created table and migrated

---
+++ database/migrations/2021_03_26_131521_ceate_card_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CeateCardTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('card', function (Blueprint $table) {
+
+        $table->string('name',100);
+        $table->string('model',100);
+        $table->float('price', 8,2);
+        $table->date('year');
+
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
